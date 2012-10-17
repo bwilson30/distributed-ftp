c1c0d05eca42dfc2ea08e1f7b655f7093784f7cd
rejuxst <rejuxst@f42a1c05-4a8e-4b1a-8e26-0390ab7237a3>
2012-10-17 22:37:25 +0000

    Updated Messaging.h

--- messaging/cpp/Messaging.h
+++ messaging/cpp/Messaging.h
@@ -8,15 +8,37 @@
 #ifndef MESSAGING_H_
 #define MESSAGING_H_
 
+#define TERM_CHAR TERMINATING_CHARACTER 
 #include "Client.h"
 
 class Messaging {
 public:
-	int get(const char* ip_addr, const char* local_path, const char* remote_path);
-	int put(const char* ip_addr, const char* local_path, const char* remote_path);
-	int ls(const char* ip_addr, const char* local_path, const char* remote_path);
-	int mkdir(const char* ip_addr, const char* remote_path);
-	int rmdir(const char* ip_addr, const char* remote_path);
+	~Messaging(){};
+	Messaging(unsigned int pn){port = pn;}; //
+	void setPort(unsigned int p);
+	void setEncrypter();		// TODO: NEED to define
+/////////////////// Communication commands ////////////////////////////
+	// The get() command takes in a local_path which will be the
+	// name of the temporary_file that it will write remote_path
+	// to. It will return the file size if sucessful and a negative
+	// number that represents the error code if false 
+	int get(const char* local_path,  const char* remote_path);
+		// Errors for get():
+		// File not Found
+		// Timeout
+		// Non-authenticated user
+	int ls( const char* local_path,  const char* remote_path);
+	int put(  const char* local_path,  const char* remote_path);
+		// Errors for put():
+		// File can't be written
+		// Timeout
+		// Non-authenticated user
+	int mkdir(const char* directory,   const char* remote_path);
+	int rmdir(const char* remote_path);	// remote_path is the full path of the Directory being deleted
+	int rm(const char*remote_path);		// remote_path is the full path of the file being deleted (Single files only)
+///////////////////////////////////////////////////////////////////////
+private:
+	unsigned int port;
 };
 
 #endif /* MESSAGING_H_ */
