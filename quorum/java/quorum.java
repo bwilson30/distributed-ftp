0c2cafb15e3ddc5940e90644f09a790f26a278c0
vind.1989@gmail.com <vind.1989@gmail.com@f42a1c05-4a8e-4b1a-8e26-0390ab7237a3>
2012-11-07 06:30:47 +0000

    Some minor changes to quorum protocol. Polished some rough edges. Works perfect now..

--- quorum/java/quorum.java
+++ quorum/java/quorum.java
@@ -5,6 +5,9 @@
 import java.io.*;
 import java.lang.*;
 import java.math.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.util.*;
 
 
@@ -40,6 +43,26 @@ class QFILE{
     	return this.serverID;
     }
     
+    public String toString(){
+    	FileInputStream stream = null;
+    	MappedByteBuffer bb = null;
+    try{
+    	try {
+    		stream = new FileInputStream(new File(fileptr.getPath()));
+    	    FileChannel fc = stream.getChannel();
+    	    bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+    	    /* Instead of using default, pass in a decoder. */
+    	    
+    	  }
+    	  
+    	  finally {
+    	    stream.close();
+    	  }
+    }
+    catch (Exception e){}
+    return Charset.defaultCharset().decode(bb).toString();	  
+    }
+    
     public QFILE(File fileptr, long tstamp, int sID){
     	this.fileptr = fileptr;
     	fileTStamp = new tStamp(tstamp);
@@ -71,7 +94,7 @@ public class quorum{
     public int quorum_files(QFILE [] files, int server_count, String dest_path){
     	if(!(server_count > 0)) return -1;
 
-    	int maxSimilarity = 0;
+    	int maxSimilarity = 1;
     	int i, count=0;
     	//long grTimestampsarry[] = new long[server_count];
     	String [] grTimestampsarry = new String[server_count];
@@ -86,7 +109,7 @@ public class quorum{
 
     	for(i=0; i<server_count; i++)
     		if(grTimestamps[i] == 1) count++;
-
+    	
     	if(count == 1){
     		int num_bytes=0;
     		for(i=0; i<server_count; i++){
@@ -125,7 +148,7 @@ public class quorum{
     	//files whose hashes are equal. If we don't have a majority, we return an error code.
     	try{
     	for(i=0; i<server_count; i++)
-    		if(grTimestamps[i] == 1) grTimestampsarry[i] = filehash.computeHash(files[i].getfile());
+    		if(grTimestamps[i] == 1) grTimestampsarry[i] = files[i].toString();//filehash.computeHash(files[i].getfile());
     		else grTimestampsarry[i] = "-1";
     	}
     	catch(Exception e){return -1;}
@@ -134,16 +157,28 @@ public class quorum{
 
     	//Compare hashes and update similarity index for each file.
     	for(i=0; i<server_count; i++){
+    		System.out.println("grTimestamps["+i+"]="+grTimestamps[i]);
     		if(grTimestamps[i] == 1)
     		for(int j=0; j<server_count; j++)
     		{
-    			if(grTimestamps[j] == 1 && i != j && grTimestampsarry[i].equals(grTimestampsarry[j])) files[i].similarityIndex++;
+    			System.out.println("grTimestamps["+j+"]="+ grTimestamps[j]+ "grTimestampsarry["+i+"]=" + grTimestampsarry[i] + "grTimestampsarry["+j+"]=" + grTimestampsarry[j]);
+    			if(grTimestamps[j] == 1  && grTimestampsarry[i].equals(grTimestampsarry[j])) files[i].similarityIndex++;
     		}
     	}
 
     	for(i=0; i<server_count; i++)
     		if(grTimestamps[i] == 1 && files[i].similarityIndex > maxSimilarity) maxSimilarity = files[i].similarityIndex;
 
+    	for(i=0; i<server_count; i++){System.out.println("MAX SIMILARITY ="+maxSimilarity+"; files["+i+"] similarity = "+files[i].similarityIndex);}
+    	
+    	int majority = (int)Math.floor(server_count/2) + 1;
+    	int num_similar_responses=0;
+    	for(i=0; i<server_count; i++){
+    		if(grTimestamps[i] == 1 && files[i].similarityIndex == maxSimilarity) num_similar_responses++;
+    	}
+    	
+    	if(num_similar_responses < majority || maxSimilarity == 1){System.err.println("No consensus! Not writing output file."); return -1;}
+    		
     	int num_bytes=0;
     	for(i=0; i<server_count; i++){
     		if(files[i].similarityIndex == maxSimilarity)/*Write files[i]->fileptr to dest path. */
@@ -182,11 +217,15 @@ public class quorum{
     	File f1 = new File("C:/Users/Vinay Bharadwaj/workspace/quorum/src/1.txt.txt");
     	File f2 = new File("C:/Users/Vinay Bharadwaj/workspace/quorum/src/2.txt.txt");
     	File f3 = new File("C:/Users/Vinay Bharadwaj/workspace/quorum/src/3.txt.txt");
-    	QFILE [] files = new QFILE[3];
+    	File f4 = new File("C:/Users/Vinay Bharadwaj/workspace/quorum/src/4.txt.txt");
+    	File f5 = new File("C:/Users/Vinay Bharadwaj/workspace/quorum/src/5.txt.txt");
+    	QFILE [] files = new QFILE[5];
     	files[0]= new QFILE(f1, 1 ,1);
     	files[1]= new QFILE(f2, 1, 2);
     	files[2]= new QFILE(f3, 1, 3);
-    	quorum q1 = new quorum(files, 3, "C:/Users/Vinay Bharadwaj/workspace/quorum/src/output.txt");
+    	files[3] = new QFILE(f4, 1, 4);
+    	files[4] = new QFILE(f5, 1, 5);
+    	quorum q1 = new quorum(files, 5, "C:/Users/Vinay Bharadwaj/workspace/quorum/src/output.txt");
     	
     }
 }
