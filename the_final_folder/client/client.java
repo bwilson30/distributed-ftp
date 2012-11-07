ceb50d77eb279bcd1f669207f7e8532a1a60ac14
rejuxst <rejuxst@f42a1c05-4a8e-4b1a-8e26-0390ab7237a3>
2012-11-07 01:40:38 +0000

    removed the wierd get directory

--- the_final_folder/client/client.java
+++ the_final_folder/client/client.java
@@ -99,7 +99,7 @@ public class client{
 		// File not found is -1
 		// Incorrect command string sent -2
 		String[] local_path = new String[qur_size];// = lwd + ".temp_qurac/";
-		 for(int i = 0; i< qur_size; i++){ local_path[i] = lwd + "/.temp_qurac/" + 'a'+i; }
+		 for(int i = 0; i< qur_size; i++){ local_path[i] = lwd /*+ "/.temp_qurac/"*/ + "/" + 'a'+i; }
 		 for(int i = 0; i< qur_size; i++){
                         servers[i].get(local_path[i] + "/" + argv[0], rwd + "/" + argv[1]);
 		}
